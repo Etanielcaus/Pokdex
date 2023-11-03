7eb2512dae05ff254d56ec3533a1c90e60e2adc7
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-03 06:42:50 -0300

    primeiro fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("Funcionou");
+const offset = 0;
+const limit = 10;
+const URL = 'https://pokeapi.co/api/v2/pokemon?limit=$(limit)&offset=$(offset)';
+
+// Ele faz o request de forma assíncrona, o código está puxando corretamente os Pokemons
+fetch(URL).then(function (response){
+    console.log(response);
+});
