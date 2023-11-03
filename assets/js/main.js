9f43e03dc81a180baa1ccc199a97d0feaafbb288
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-03 07:00:54 -0300

    estudos callback

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const limit = 10;
 const URL = 'https://pokeapi.co/api/v2/pokemon?limit=$(limit)&offset=$(offset)';
 
 // Ele faz o request de forma assíncrona, o código está puxando corretamente os Pokemons
-fetch(URL).then(function (response){
-    console.log(response);
-});
+fetch(URL)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
